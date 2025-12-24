7dc8e918706727ad5a05e80e47862d867fc458df
Jun <imlijunda@gmail.com>
2025-12-24 11:18:20 +1030

    Initial Commit

---
+++ GotoBed/GotoBedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GotoBedApp.swift
+//  GotoBed
+//
+//  Created by Junda Li on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GotoBedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
